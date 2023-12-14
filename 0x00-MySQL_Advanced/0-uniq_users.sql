9f3b8d7a7f8e6cea946f34f7d3b2aaa692f8baaf
Paul John <rainson.work@gmail.com>
2023-12-14 19:45:43 +0300

    script to create a users table

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- Create a table users with 3 columns
+CREATE TABLE IF NOT EXISTS users(
+	id	  INT PRIMARY KEY
+		  AUTO_INCREMENT	  NOT NULL,
+	email VARCHAR(255) UNIQUE NOT NULL,
+	name  VARCHAR(255)
+);
